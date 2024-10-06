6b260fc5c67c612db4b15c43677291cf9aaca8b2
syoch <syoch64@gmail.com>
2024-10-06 10:59:11 +0000

    Update(mbed-ce): Changed download depth to 1

--- cmake/Findmbed-ce.cmake
+++ cmake/Findmbed-ce.cmake
@@ -3,6 +3,7 @@ include(FetchContent)
 FetchContent_Populate(mbed-ce
   GIT_REPOSITORY git@github.com:mbed-ce/mbed-os.git
   GIT_TAG 3297baedff11f68657f656d17ec738ee59ea3e8f
+  GIT_SHALLOW 1
   SOURCE_DIR ${CMAKE_BINARY_DIR}/3rd-party/mbed-ce/src
   BINARY_DIR ${CMAKE_BINARY_DIR}/3rd-party/mbed-ce/build
   SUBBUILD_DIR ${CMAKE_BINARY_DIR}/3rd-party/mbed-ce/subbuild
