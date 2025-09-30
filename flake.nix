3cf1dc03a7884df68da604ecb07a55ef0b1eb9fb
syoch <syoch64@gmail.com>
2025-09-30 14:27:08 +0900

    Feat: Nix Flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "libs-cmake";
+        version = "0.1.0";
+
+        src = ./.;
+
+        nativeBuildInputs = [ pkgs.cmake ];
+      };
+    };
+}
