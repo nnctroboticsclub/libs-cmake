4da11ea850ffa625a66366f870501f7388c4ae01
syoch <syoch64@gmail.com>
2025-11-16 13:56:59 +0900

    Update: Removed MBedOS support from libs-cmake
    Use StaticMBedOS instead.

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
             pkgs.cmake
           ];
           buildInputs = [
-            mbed-os
+            # mbed-os
           ];
         };
 
@@ -30,69 +30,32 @@
           src = ./.;
 
           buildPhase =
-            let prefix = "${pkgs.gcc-arm-embedded}/bin/arm-none-eabi";
-            in ''
-            cat <<EOF > GccArmToolchain.cmake
-            set(CMAKE_SYSTEM_NAME Generic)
-            set(CMAKE_SYSTEM_PROCESSOR arm)
+            let
+              prefix = "${pkgs.gcc-arm-embedded}/bin/arm-none-eabi";
+            in
+            ''
+              cat <<EOF > GccArmToolchain.cmake
+              set(CMAKE_SYSTEM_NAME Generic)
+              set(CMAKE_SYSTEM_PROCESSOR arm)
 
-            set(CMAKE_C_COMPILER "${prefix}-gcc")
-            set(CMAKE_CXX_COMPILER "${prefix}-g++")
-            set(CMAKE_ASM_COMPILER "${prefix}-gcc")
-            set(CMAKE_OBJCOPY "${prefix}-objcopy")
-            set(CMAKE_OBJDUMP "${prefix}-objdump")
-            set(CMAKE_SIZE "${prefix}-size")
-            set_property(GLOBAL PROPERTY ELF2BIN "${prefix}-objcopy")
+              set(CMAKE_C_COMPILER "${prefix}-gcc")
+              set(CMAKE_CXX_COMPILER "${prefix}-g++")
+              set(CMAKE_ASM_COMPILER "${prefix}-gcc")
+              set(CMAKE_OBJCOPY "${prefix}-objcopy")
+              set(CMAKE_OBJDUMP "${prefix}-objdump")
+              set(CMAKE_SIZE "${prefix}-size")
+              set_property(GLOBAL PROPERTY ELF2BIN "${prefix}-objcopy")
 
-            set(CMAKE_C_COMPILER_WORKS 1)
-            set(CMAKE_CXX_COMPILER_WORKS 1)
-            set(CMAKE_ASM_COMPILER_WORKS 1)
-            EOF
-          '';
+              set(CMAKE_C_COMPILER_WORKS 1)
+              set(CMAKE_CXX_COMPILER_WORKS 1)
+              set(CMAKE_ASM_COMPILER_WORKS 1)
+              EOF
+            '';
 
           installPhase = ''
             mkdir -p $out/lib/cmake
 
             cp GccArmToolchain.cmake $out/lib/cmake
-          '';
-        };
-
-        mbed-os-src = pkgs.stdenv.mkDerivation {
-          pname = "mbed-os-src";
-          version = "0.1.0";
-
-          src = builtins.fetchGit {
-            url = "https://github.com/mbed-ce/mbed-os.git";
-            ref = "master";
-            rev = "4ba00162ba2d73c64583018983391e1dfeaee83d";
-          };
-
-          installPhase = ''
-            cp -r . $out
-          '';
-        };
-
-        mbed-os = pkgs.stdenv.mkDerivation {
-          pname = "mbed-os";
-          version = "0.1.0";
-
-          src = ./mbed-os;
-
-          installPhase = ''
-            mkdir -p $out/lib/cmake
-
-            cp MbedCE-Toolchain.cmake $out/lib/cmake
-            cp MbedCE.cmake $out/lib/cmake
-
-            cat <<EOF > $out/lib/cmake/Findmbed-ce.cmake
-            set(mbed-ce_SOURCE_DIR ${mbed-os-src})
-
-            include(FindPackageHandleStandardArgs)
-            find_package_handle_standard_args(mbed-ce
-              REQUIRED_VARS
-                mbed-ce_SOURCE_DIR
-            )
-            EOF
           '';
         };
 
