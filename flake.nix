f13cec639408785a65368990fad0106487f6c461
syoch <syoch64@gmail.com>
2025-11-16 09:05:21 +0900

    Update: Nixified

--- flake.nix
+++ flake.nix
@@ -15,8 +15,48 @@
 
           src = ./.;
 
-          nativeBuildInputs = [ pkgs.cmake ];
+          nativeBuildInputs = [
+            pkgs.cmake
+          ];
+          buildInputs = [
+            mbed-os
+          ];
         };
+
+        gcc-arm-toolchain = pkgs.stdenv.mkDerivation {
+          pname = "gcc-arm-toolchain";
+          version = "0.1.0";
+
+          src = ./.;
+
+          buildPhase =
+            let prefix = "${pkgs.gcc-arm-embedded}/bin/arm-none-eabi";
+            in ''
+            cat <<EOF > GccArmToolchain.cmake
+            set(CMAKE_SYSTEM_NAME Generic)
+            set(CMAKE_SYSTEM_PROCESSOR arm)
+
+            set(CMAKE_C_COMPILER "${prefix}-gcc")
+            set(CMAKE_CXX_COMPILER "${prefix}-g++")
+            set(CMAKE_ASM_COMPILER "${prefix}-gcc")
+            set(CMAKE_OBJCOPY "${prefix}-objcopy")
+            set(CMAKE_OBJDUMP "${prefix}-objdump")
+            set(CMAKE_SIZE "${prefix}-size")
+            set_property(GLOBAL PROPERTY ELF2BIN "${prefix}-objcopy")
+
+            set(CMAKE_C_COMPILER_WORKS 1)
+            set(CMAKE_CXX_COMPILER_WORKS 1)
+            set(CMAKE_ASM_COMPILER_WORKS 1)
+            EOF
+          '';
+
+          installPhase = ''
+            mkdir -p $out/lib/cmake
+
+            cp GccArmToolchain.cmake $out/lib/cmake
+          '';
+        };
+
         mbed-os-src = pkgs.stdenv.mkDerivation {
           pname = "mbed-os-src";
           version = "0.1.0";
@@ -24,13 +64,14 @@
           src = builtins.fetchGit {
             url = "https://github.com/mbed-ce/mbed-os.git";
             ref = "master";
-            rev = "17dc3dc2e6e2817a8bd3df62f38583319f0e4fed";
+            rev = "4ba00162ba2d73c64583018983391e1dfeaee83d";
           };
 
           installPhase = ''
             cp -r . $out
           '';
         };
+
         mbed-os = pkgs.stdenv.mkDerivation {
           pname = "mbed-os";
           version = "0.1.0";
@@ -43,16 +84,33 @@
             cp MbedCE-Toolchain.cmake $out/lib/cmake
             cp MbedCE.cmake $out/lib/cmake
 
-            cat <<EOF > $out/lib/cmake/FindMbedOS.cmake
-            set(mbed-os_SOURCE_DIR ${mbed-os-src})
+            cat <<EOF > $out/lib/cmake/Findmbed-ce.cmake
+            set(mbed-ce_SOURCE_DIR ${mbed-os-src})
 
             include(FindPackageHandleStandardArgs)
-            find_package_handle_standard_args(mbed-os
+            find_package_handle_standard_args(mbed-ce
               REQUIRED_VARS
-                mbed-os_SOURCE_DIR
+                mbed-ce_SOURCE_DIR
             )
             EOF
           '';
+        };
+
+        test = pkgs.stdenv.mkDerivation {
+          pname = "libs-cmake-test";
+          version = "0.1.0";
+
+          src = ./test;
+
+          nativeBuildInputs = [
+            pkgs.cmake
+            libs-cmake
+          ];
+
+          cmakeFlags = [
+            "-DCMAKE_BUILD_TYPE=Debug"
+            "-DCMAKE_MODULE_PATH=${libs-cmake}/lib/cmake"
+          ];
         };
       };
     };
--- mbed-os/MbedCE.cmake
+++ mbed-os/MbedCE.cmake
@@ -1,4 +1,6 @@
-include(MbedCE-Toolchain)
+# Called be after project()
 
-find_package(mbed-ce REQUIRED)
+# Assume MBedCE-Toolchain is already included before project()
+
+include(mbed_project_setup)
 add_subdirectory(${mbed-ce_SOURCE_DIR} mbed-ce)
